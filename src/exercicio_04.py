92bbe0fdbe52c7a1bb76a6671ef8785fd61b450f
Paulo Patricio <paulocdn99@gmail.com>
2026-04-01 23:00:22 -0300

    Exercicio 04 ok

--- src/exercicio_04.py
+++ src/exercicio_04.py
@@ -1,11 +1,6 @@
+from os import name
+
+
 def format_name(name: str) -> tuple[str, str, str]:
-    """
-    Retorna o nome em diferentes formatos.
+    return (name.lower(), name.upper(), name.capitalize())
 
-    Args:
-        name (str): nome de entrada
-
-    Returns:
-        tuple[str, str, str]: (lowercase, uppercase, titlecase)
-    """
-    pass
